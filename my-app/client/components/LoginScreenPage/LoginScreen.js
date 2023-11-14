adc26c50f432ec27c5bca57091e1f28b6b58228d
Nathaniel Kim <nathanielk0124@gmail.com>
2023-11-14 01:49:28 -0500

    Added Registration and Login (need to reroute login to chat)

--- my-app/client/components/LoginScreenPage/LoginScreen.js
+++ my-app/client/components/LoginScreenPage/LoginScreen.js
@@ -1,12 +1,26 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, Text } from 'react-native';
+import axios from 'axios';
 
 const LoginScreen = ( { navigation } ) => {
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState ('');
+    const [password, setPassword] = useState('');
 
-    const handleLogin = () => {
-        // Implement logic
+    const [loginError, setLoginError] = useState('');
+
+    const IP_ADDRESS = "10.0.0.16";
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        
+        try {
+            const response = await axios.post('http://' + IP_ADDRESS + ':5001/login', {email, password})
+            const data = response.data;
+            console.log("Logged In!");
+
+        } catch (error) {
+            setLoginError(error.response.data.message);
+        }
     };
 
     return (
@@ -14,9 +28,14 @@ const LoginScreen = ( { navigation } ) => {
             <TextInput placeholder="Email" onChangeText={setEmail} value={email} />
             <TextInput placeholder="Password" onChangeText={setPassword} value={password} secureTextEntry />
             <Button title="Login" onPress={handleLogin} />
+            {loginError ? <Text style={{ color: 'red'}}>{loginError}</Text> : null}
             <Text onPress={() => navigation.navigate('Register')}>Register</Text>
         </View>
     );
 };
+
+// const styles = StyleSheet.create({
+
+// });
 
 export default LoginScreen;
--- my-app/server/server.js
+++ my-app/server/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const registerRoutes = require("./routes/registerRoutes");
+const loginRoutes = require("./routes/loginRoutes");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Incorporates routes to the express instance
 app.use(registerRoutes);
+app.use(loginRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
